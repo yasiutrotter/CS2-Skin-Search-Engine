d3a87d41ea09b2a70ba24f125b4e71b9b677bf65
yasiutrotter <veenquu@gmail.com>
2025-01-15 13:15:03 +0100

    [frontend] update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,89 +1,53 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import { Navbar } from "./components/Navbar";
 
 type Skin = {
-  weapon: {
-    name: string;
-  },
-  pattern: {
-    name: string;
-  },
+  weapon: string;
   image: string;
-  category: {
-    name: string;
-  },
+  finish: string;
 };
 
 export default function Home() {
   const [skins, setSkins] = useState<Skin[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState("All");
   const [query, setQuery] = useState("");
   const [filteredSkins, setFilteredSkins] = useState<Skin[]>([]);
-
   useEffect(() => {
-    fetch("https://bymykel.github.io/CSGO-API/api/en/skins.json")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Failed to fetch skins data");
-        }
-        return res.json();
-      })
-      .then((data: any[]) => {
-        const transformedData = data
-          .filter(
-            (skin) =>
-              skin.weapon && skin.weapon.name && skin.pattern && skin.pattern.name && skin.category && skin.category.name
-          ) // Filter out invalid skins
-          .map((skin) => ({
-            weapon: {
-              name: skin.weapon.name,
-            },
-            pattern: {
-              name: skin.pattern.name,
-            },
-            image: skin.image,
-            category: {
-              name: skin.category.name,
-            },
-          }));
+    fetch("https://raw.githubusercontent.com/qwkdev/csapi/main/data2.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const transformedData = Object.entries(data).map(([key, value]: [string, any]) => ({
+          weapon: value.weapon,
+          image: value.image,
+          finish: value.finish,
+        }));
 
         const uniqueSkins = Array.from(
-          new Map(
-            transformedData.map((skin) => [
-              `${skin.weapon.name} | ${skin.pattern.name}`,
-              skin,
-            ])
-          ).values()
+          new Map(transformedData.map(skin => [`${skin.weapon} | ${skin.finish}`, skin])).values()
         );
         setSkins(uniqueSkins);
-      })
-      .catch((error) => {
-        console.error("Error fetching skins:", error);
       });
   }, []);
 
   useEffect(() => {
-    // Combined filtering logic
-    const filtered = skins.filter((skin) => {
-      const matchesCategory = selectedCategory === "All" || skin.category.name === selectedCategory;
-      const matchesQuery =
-        query.trim() === "" ||
-        skin.weapon.name.toLowerCase().includes(query.toLowerCase()) ||
-        skin.pattern.name.toLowerCase().includes(query.toLowerCase());
+    if (query.trim() === "") {
+      setFilteredSkins([]);
+      return;
+    }
 
-      return matchesCategory && matchesQuery;
-    });
+    setFilteredSkins(
+      skins.filter((skin) =>
+        skin.weapon.toLowerCase().includes(query.toLowerCase()) ||
+        skin.finish.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+  }, [query, skins]);
 
-    setFilteredSkins(filtered);
-  }, [query, selectedCategory, skins]); // Trigger whenever skins, query, or selectedCategory changes
-
-  const router = useRouter();
+  const router = useRouter()
   return (
     <div className="px-96 my-8 bg-gray-950">
-      <Navbar />
+      <Navbar/>
       <div className="flex flex-col items-center justify-center mt-16">
         <h1 className="text-5xl font-extrabold text-gray-50 mb-2">CS2 Skin Search Engine</h1>
         <h3 className="text-xl font-thin text-gray-400">Search any existing CS2 skin and test it in game!</h3>
@@ -92,47 +56,30 @@ export default function Home() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Enter a skin or weapon name"
-          className="pl-8 mt-20 p-4 w-full rounded-full text-sm font-thin place-content-center bg-gray-900 text-gray-50 decoration-transparent border-2 border-gray-800 outline-none mb-4"
-        />
+          className="pl-8 mt-20 p-4 w-full rounded-full text-sm font-thin place-content-center bg-gray-900 text-gray-50 decoration-transparent border-2 border-gray-800 outline-none mb-4"></input>
 
-        <select onChange={(e) => setSelectedCategory(e.target.value)} value={selectedCategory}>
-          <option value="All">All</option>
-          <option value="Pistols">Pistols</option>
-          <option value="Rifles">Rifles</option>
-        </select>
       </div>
-
       {query.trim().length < 3 ? (
         <div className="container mx-auto">
-          <div className="grid grid-cols-2 gap-4"></div>
+          <div className="grid grid-cols-2 gap-4">
+
+          </div>
+          
         </div>
       ) : (
         <div className="container mx-auto">
           <div className="grid grid-cols-3 gap-4">
-            {filteredSkins.map((skin, index) => (
-              <div
-                key={index}
-                className="flex-col bg-gray-900 border-2 border-gray-800 flex items-center justify-center p-2 py-6 rounded-2xl cursor-pointer hover:bg-gray-800 transition-colors duration-300 ease-in-out"
-                onClick={() =>
-                  router.push(
-                    `/skin/${encodeURIComponent(skin.weapon.name).toLowerCase()}/${encodeURIComponent(skin.pattern.name).toLowerCase()}`
-                  )
-                }
-              >
-                <img
-                  src={skin.image || "/fallback-image-path.jpg"}
-                  loading="lazy"
-                  alt={`${skin.weapon.name} ${skin.pattern.name}`}
-                  className="w-48 flex h-auto mb-2"
-                />
-                <p className="text-center font-extrabold text-3xl">{skin.weapon.name}</p>
-                <p className="text-center font-medium text-xl">{skin.pattern.name}</p>
-              </div>
-            ))}
-            {filteredSkins.length === 0 && (
-              <p className="text-gray-400 text-center col-span-full mt-4">No results found for your query.</p>
-            )}
-          </div>
+          {filteredSkins.map((skin, index) => (
+            <div key={index} className="flex-col bg-gray-900 border-2 border-gray-800 flex items-center justify-center p-2 py-6 rounded-xl cursor-pointer hover:bg-gray-800 transition-colors duration-300 ease-in-out" onClick={() => router.push(`/skin/${encodeURIComponent(skin.weapon).toLowerCase()}/${encodeURIComponent(skin.finish).toLowerCase()}`)}>
+              <img src={skin.image} alt={`${skin.weapon} ${skin.finish}`} className="w-48 flex h-auto mb-2" />
+              <p className="text-center font-extrabold text-3xl">{skin.weapon}</p>
+              <p className="text-center font-medium text-xl">{skin.finish}</p>
+            </div>
+          ))}
+          {filteredSkins.length === 0 && (
+            <p className="text-gray-400 text-center col-span-full mt-4">No results found for your query.</p>
+          )}
+        </div>
         </div>
       )}
     </div>
