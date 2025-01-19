7bca00ac912dc2bfe97bc64e7720c154f5c3fd9d
Jan Kempa <veenquu@gmail.com>
2025-01-19 16:06:16 +0100

    [logic] fixed skin searching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import { Navbar } from "./components/Navbar";
+import Image from 'next/image';
 
 type Skin = {
   weapon: string;
+  pattern: string; 
   image: string;
-  finish: string;
 };
 
 export default function Home() {
@@ -14,17 +15,19 @@ export default function Home() {
   const [query, setQuery] = useState("");
   const [filteredSkins, setFilteredSkins] = useState<Skin[]>([]);
   useEffect(() => {
-    fetch("https://raw.githubusercontent.com/qwkdev/csapi/main/data2.json")
+    fetch("https://bymykel.github.io/CSGO-API/api/en/skins.json")
       .then((res) => res.json())
       .then((data) => {
-        const transformedData = Object.entries(data).map(([key, value]: [string, any]) => ({
-          weapon: value.weapon,
+        const transformedData = Object.entries(data).map(([_, value]: [string, any]) => ({
+          weapon: value.weapon?.name?.toString() || "", 
           image: value.image,
-          finish: value.finish,
+          pattern: value.pattern?.name?.toString() || "",
         }));
 
         const uniqueSkins = Array.from(
-          new Map(transformedData.map(skin => [`${skin.weapon} | ${skin.finish}`, skin])).values()
+          new Map(
+            transformedData.map(skin => [`${skin.weapon} | ${skin.pattern}`, skin])
+          ).values()
         );
         setSkins(uniqueSkins);
       });
@@ -39,15 +42,15 @@ export default function Home() {
     setFilteredSkins(
       skins.filter((skin) =>
         skin.weapon.toLowerCase().includes(query.toLowerCase()) ||
-        skin.finish.toLowerCase().includes(query.toLowerCase())
+        skin.pattern.toLowerCase().includes(query.toLowerCase())
       )
     );
   }, [query, skins]);
 
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className="px-96 my-8 bg-gray-950">
-      <Navbar/>
+      <Navbar />
       <div className="flex flex-col items-center justify-center mt-16">
         <h1 className="text-5xl font-extrabold text-gray-50 mb-2">CS2 Skin Search Engine</h1>
         <h3 className="text-xl font-thin text-gray-400">Search any existing CS2 skin and test it in game!</h3>
@@ -56,30 +59,38 @@ export default function Home() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Enter a skin or weapon name"
-          className="pl-8 mt-20 p-4 w-full rounded-full text-sm font-thin place-content-center bg-gray-900 text-gray-50 decoration-transparent border-2 border-gray-800 outline-none mb-4"></input>
-
+          className="pl-8 mt-20 p-4 w-full rounded-full text-sm font-thin place-content-center bg-gray-900 text-gray-50 decoration-transparent border-2 border-gray-800 outline-none mb-4"
+        ></input>
       </div>
       {query.trim().length < 3 ? (
         <div className="container mx-auto">
-          <div className="grid grid-cols-2 gap-4">
-
-          </div>
-          
+          <div className="grid grid-cols-2 gap-4"></div>
         </div>
       ) : (
         <div className="container mx-auto">
           <div className="grid grid-cols-3 gap-4">
-          {filteredSkins.map((skin, index) => (
-            <div key={index} className="flex-col bg-gray-900 border-2 border-gray-800 flex items-center justify-center p-2 py-6 rounded-xl cursor-pointer hover:bg-gray-800 transition-colors duration-300 ease-in-out" onClick={() => router.push(`/skin/${encodeURIComponent(skin.weapon).toLowerCase()}/${encodeURIComponent(skin.finish).toLowerCase()}`)}>
-              <img src={skin.image} alt={`${skin.weapon} ${skin.finish}`} className="w-48 flex h-auto mb-2" />
-              <p className="text-center font-extrabold text-3xl">{skin.weapon}</p>
-              <p className="text-center font-medium text-xl">{skin.finish}</p>
-            </div>
-          ))}
-          {filteredSkins.length === 0 && (
-            <p className="text-gray-400 text-center col-span-full mt-4">No results found for your query.</p>
-          )}
-        </div>
+            {filteredSkins.map((skin, index) => (
+              
+              <div
+                key={index}
+                className="flex-col bg-gray-900 border-2 border-gray-800 flex items-center justify-center p-2 py-6 rounded-xl cursor-pointer hover:bg-gray-800 transition-colors duration-300 ease-in-out"
+                onClick={() =>
+                  router.push(
+                    `/skin/${encodeURIComponent(skin.weapon).toLowerCase()}/${encodeURIComponent(skin.pattern).toLowerCase()}`
+                  )
+                }
+              >
+                <Image src={skin.image} alt={`${skin.weapon} ${skin.pattern}`} width={192} height={192} loading="lazy" quality={75} className="flex h-auto mb-2"/>
+                
+                
+                <p className="text-center font-extrabold text-3xl mt-6">{skin.weapon}</p>
+                <p className="text-center font-medium text-xl">{skin.pattern}</p>
+              </div>
+            ))}
+            {filteredSkins.length === 0 && (
+              <p className="text-gray-400 text-center col-span-full mt-4">No results found for your query.</p>
+            )}
+          </div>
         </div>
       )}
     </div>
--- src/app/skin/[weapon]/[finish]/page.tsx
+++ src/app/skin/[weapon]/[finish]/page.tsx
@@ -1,63 +1,88 @@
 "use client";
 import { HamburgerMenu } from "@/app/components/HamburgerMenu";
-import { useParams, useRouter } from "next/navigation"; // do pobierania parametrów z URL
+import { useParams, useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 
 type Skin = {
-  weapon: {
-    name: string;
-  },
+  weapon: string;
+  pattern: string; 
   image: string;
-  pattern: {
-    name: string;
-  },
-  category: {
-    name: string;
-  },
 };
 
 export default function SkinDetails() {
-  const { weapon, pattern } = useParams(); // Pobieramy dynamiczne parametry URL: weapon i finish
+  const { weapon, pattern } = useParams();
   const [skin, setSkin] = useState<Skin | null>(null);
-  const router = useRouter(); 
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
-      fetch("https://raw.githubusercontent.com/qwkdev/csapi/main/data2.json")
-        .then((res) => res.json())
-        .then((data) => {
-          const transformedData = Object.entries(data).map(([key, value]: [string, any]) => ({
-            weapon: {
-              name: value.weapon.name,
-            },
-            pattern: {
-              name: value.pattern.name,
-            },
-            image: value.image,
-          }));
+    setIsLoading(true);
 
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-          setSkin(null);
-        });
-    }, [weapon, pattern]);
+    fetch("https://bymykel.github.io/CSGO-API/api/en/skins.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const transformedData = Object.entries(data).map(([_, value]: [string, any]) => ({
+          weapon: value.weapon?.name?.toString() || "",
+          image: value.image,
+          pattern: value.pattern?.name?.toString() || "",
+        }));
 
-  if (!skin) {
+        const foundSkin = transformedData.find(
+          (item) => 
+            item.weapon.toLowerCase() === decodeURIComponent(weapon as string).toLowerCase() &&
+            item.pattern.toLowerCase() === decodeURIComponent(pattern as string).toLowerCase()
+        );
+
+        setSkin(foundSkin || null);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setSkin(null);
+        setIsLoading(false);
+      });
+  }, [weapon, pattern]);
+
+  if (isLoading) {
     return (
-      <div className="container mx-auto px-48 py-6">
-        <HamburgerMenu />
-        <h1 className="text-2xl font-bold mb-4">Nie znaleziono skina</h1>
-        <p className="text-gray-500 text-center">Brak danych dla podanego skina.</p>
+      <div className="min-h-screen flex flex-col items-center justify-center mx-auto px-48">
+        <div className="w-32 h-32 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+        <p className="mt-4 text-gray-500">Loading skin details...</p>
       </div>
     );
   }
 
+
+  if (!skin) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center mx-auto px-48 gap-2">
+        <h1 className="text-5xl font-bold">Error!</h1>
+        <p className="text-gray-500 text-2xl text-center">
+          It seems like the skin you tried to search doesn't exist.
+        </p>
+        <button 
+          onClick={() => router.push('/')}
+          className="text-white text-center font-medium hover:text-primary-700 duration-200 ease-in-out"
+        >
+          Go back
+        </button>
+      </div>
+    );
+  }
+
+
   return (
     <div className="container mx-auto px-48 py-6">
       <HamburgerMenu />
-      <div className="flex flex-col items-center justify-center ">
-        <h1 className="text-2xl font-bold mb-4">{skin.weapon.name} | {skin.pattern.name}</h1>
-        <img src={skin.image} alt={`${skin.weapon.name} ${skin.pattern.name}`} className="w-128 h-auto mb-4" />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold mb-4">
+          {skin.weapon} | {skin.pattern}
+        </h1>
+        <img 
+          src={skin.image} 
+          alt={`${skin.weapon} ${skin.pattern}`} 
+          className="w-128 h-auto mb-4"
+        />
       </div>
     </div>
   );
